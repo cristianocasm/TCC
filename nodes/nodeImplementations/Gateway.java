2c8284aaf4cca7bd7a0d209dd66f980333bd3e83
cristiano <cristiano.souza.mendonca@gmail.com>
2014-08-16 15:24:17 -0300

    Adding necessary methods (without implementation) to GatewayNode class

--- nodes/nodeImplementations/Gateway.java
+++ nodes/nodeImplementations/Gateway.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 
 import sinalgo.configuration.WrongConfigurationException;
 import sinalgo.nodes.Node;
+import sinalgo.nodes.Node.NodePopupMethod;
 import sinalgo.nodes.messages.Inbox;
 
 public class Gateway extends Node {
@@ -11,7 +12,37 @@ public class Gateway extends Node {
 	public Gateway(){
 		this.setColor(Color.BLUE);
 	}
+	
+	@NodePopupMethod(menuText = "Get Temperature")
+	public void getTemperature() {
+		// TODO
+	}
+	
+	@NodePopupMethod(menuText = "Set Temperature = 20")
+	public void setTemperature() {
+		// TODO
+	}
+	
+	@NodePopupMethod(menuText = "Get Lightness")
+	public void getLightness() {
+		// TODO
+	}
 
+	@NodePopupMethod(menuText = "Set Lightness = 100")
+	public void setLightness() {
+		// TODO
+	}
+	
+	@NodePopupMethod(menuText = "Turn Air Conditioner ON")
+	public void airConditionerON() {
+		// TODO
+	}
+	
+	@NodePopupMethod(menuText = "Turn Air Conditioner OFF")
+	public void airConditionerOFF() {
+		// TODO
+	}
+	
 	@Override
 	public void handleMessages(Inbox inbox) {
 		// TODO Auto-generated method stub
